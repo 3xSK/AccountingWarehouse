91c93fc051f06275ab3e83d466fb7a6d02c33ac7
3xSK <39564751+3xSK@users.noreply.github.com>
2021-12-13 08:08:48 -0500

    Update pending_journal.php
    Fixed display pending journal entries

--- AccountingWarehouse_V2/pending_journal.php
+++ AccountingWarehouse_V2/pending_journal.php
@@ -1,12 +1,12 @@
 <?php
-error_reporting(E_ERROR);
+//error_reporting(E_ERROR);
 session_start();
 include_once("config.php");
 $id=0;
-
-if($_SESSION['admin']) {
+/*
+if($_SESSION['group_id']!="") {
 	$Function->Redirect("index.php");
-}
+}*/
 
 if(isset($_REQUEST['action']))  {
 
@@ -26,7 +26,7 @@ if(isset($_REQUEST['action']))  {
 	}
 
 
-$accounts=$db->selectMultiRecords("select * from journal where journalStatus='Pending' ");
+$accounts=$db->selectMultiRecords("select * from journalmaster order by journal_id");
 ?>
 <?php include("pages/header.php"); ?>
 
@@ -40,7 +40,10 @@ $accounts=$db->selectMultiRecords("select * from journal where journalStatus='Pe
 
       <div class="col-xs-12 text-right">
 
-        <div class="form-group"> <a class="btn btn-primary" href="add_journal.php"> <i class="fa fa-plus"></i> Add Journal Entry</a> 
+        <div class="form-group">
+
+
+      <a class="btn btn-primary" data-bs-toggle="modal" href="add_journal.php" data-bs-target="#exampleModal">  Add Journal Entry</a> 
 
         <?php
     if($_SESSION['group_id']=='4') 
@@ -53,18 +56,18 @@ $accounts=$db->selectMultiRecords("select * from journal where journalStatus='Pe
       </div>
       </div>
 
-      <form class="form-inline" method="POST" action="search.php">
+       <div class="form-inline" method="POST" action="search.php">
         <label></label>
-        <input type="date" class="form-control" placeholder="Start"  name="date1" value="<?php echo isset($_POST['date1']) ? $_POST['date1'] : '' ?>" />
+        <input type="date" class="form-control" placeholder="Start"  id="FromDate"  />
         <label>To</label>
-        <input type="date" class="form-control" placeholder="End"  name="date2" value="<?php echo isset($_POST['date2']) ? $_POST['date2'] : '' ?>"/>
-        <button class="btn btn-primary" name="search"><span class="glyphicon glyphicon-search"></span></button> <a href="search.php" type="button" class="btn btn-success"><span class = "glyphicon glyphicon-refresh"><span></a>
-		</form>
+        <input type="date" class="form-control" placeholder="End"  id="ToDate"/> 
+        <button class="btn btn-primary" name="search" id="btnFilter"><span class="glyphicon glyphicon-search"></span></button> <a  type="button" id="btnAllSearch" class="btn btn-success"><span class = "glyphicon glyphicon-refresh"><span></a>
+		</div>
 
     </div>
 
 	 <br />
-    <?php if($_SESSION['msg']!="") { ?>
+    <?php if(isset($_SESSION['msg'])) { ?>
     <div class="<?php echo $_SESSION['msg_type']; ?>"> <?php echo $_SESSION['msg']; ?> </div>
     <?php } ?>
     <div class="row">
@@ -74,86 +77,58 @@ $accounts=$db->selectMultiRecords("select * from journal where journalStatus='Pe
           <!-- /.box-header -->
           <div class="box-body table-responsive no-padding">
             <div class="panel-body">
-             <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+              <table width="100%" class="table" >
+              
                 <thead>
                   <tr>
-                    <th>Date Created</th>
-                <th>Creator</th>
-                <th>Accounts Involved</th>
-                <th>Description</th>
-                <th>Debit</th>
-                <th>Credit</th>
-                <th>PR</th>
-                <th>Status</th>
-                <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php
-
-					
-
-                    if(!empty($accounts))
-
-
-
-                    {
-
-
-
-                        foreach($accounts as $account)
-
-
-
-                        {
-
-						
-
-						
-
-                    ?>
-					        <tr>
-                                <td><?php echo $account['datecreated']; ?></td>
-                                <td><?php echo $account['creator']; ?></td>
-                                <td><?php echo $account['accounts']; ?></td>
-                                <td><?php echo $account['entry_description']; ?></td>
-                                <td><?php echo  number_format($account['debit'],2); ?></td>
-                                <td><?php echo  number_format($account['credit'],2); ?></td>
-                                <td><?php echo $account['PR']; ?></td>
-                                <td><?php echo $account['journalStatus']; ?></td>
-                                <script>
-                                    let text = num.toLocalString();
-                                    document.getElementById("debitvalue").innerHTML=text;
-                                </script>
-                                
-                                 <td class="text-center"> <a class="btn btn-sm btn-info" href="edit_journal.php?journal_id=<?php echo $account['journal_id']; ?>" title="Edit"> <i class="fa fa-pencil"></i> </a> &nbsp;&nbsp;
-
-                                <?php
-                              if($_SESSION['group_id']=='2') 
-                              {
-                              ?>
-                                        <a class="btn btn-sm btn-danger deleteUser" onclick="return confirm('Do you really want to Delete?');" href="journal.php?action=delete&journal_id=<?php echo $account['journal_id']; ?>" title="Delete"> <i class="fa fa-trash"></i> </a>
-                                  <?php
-                                  }
-                              ?>
-
-                              
-                              </td>
-                             </tr>
+                 
+                    <th>Type</th>
+                 
+                   <th>Debit</th>
+                   <th>Credit</th>
+                   
                   
+                  
+                    
+                 </tr>
+                </thead>
+                <tbody >
                   <?php
-
-
-
-                        }
-
-
-
-                    }
-
-
+                         
+$accounts=$db->selectMultiRecords("select  j.accounts as Accounts, j.accType as Type, Sum(j.debit) as Debit, sum(j.credit) as Credit from journal j  group by j.accType order by j.accType");
+            foreach($accounts as $account){
 
                     ?>
+                    <tr >
+    <th><?php echo $account['Type'];?></th>
+                 
+                   <th class="warning">$<?php echo $account['Debit'];?></th>
+                   <th class="warning">$<?php echo $account['Credit'];?></th>
+</tr>
+                  <?php }?>
+                </tbody>
+              </table>
+             <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+              
+                <thead>
+                  <tr>
+                    <th>Journal Entry #</th>
+                    <th>Date</th>
+               
+                 
+                   
+                   <th>Description</th>
+                  
+                   <th>Action</th>
+                   <th>Entry Status</th>
+                    
+                 
+                    
+                 </tr>
+                </thead>
+                <tbody id="tblaccount">
+                   
+                  
                 </tbody>
               </table>
             </div>
@@ -165,4 +140,47 @@ $accounts=$db->selectMultiRecords("select * from journal where journalStatus='Pe
     </div>
   </section>
 </div>
+
+
 <?php include("pages/footer.php"); ?>
+<script>
+  $(document).ready(function () {
+    load('0');
+    function load(filter)
+    {
+   var FromDate=$("#FromDate").val();
+   var ToDate=$("#ToDate").val();
+      //List.php
+      var opt=filter;
+
+     // alert(filter)
+      $.ajax({
+   url:'Journal/pendingJournalList.php',
+   datatype:"application/json",
+   type:'POST',
+  data:{FromDate:FromDate,ToDate:ToDate,filter:opt}, 
+   success:function(data){
+      $('#tblaccount').html(data); 
+      console.log(data);
+   },
+   error:function( e){
+      // code for error
+          console.log(e);}
+        });
+   }
+   $("#btnFilter").click(function(){
+
+
+load('1');
+
+});
+    $("#btnAllSearch").click(function(){
+
+
+load('0');
+
+});
+   
+ });
+ 
+</script>
